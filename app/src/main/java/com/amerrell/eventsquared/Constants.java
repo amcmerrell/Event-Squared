9f956d98f4f416f948b716c6636d769726893197
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-19 11:10:30 -0800

    create getTMEvents method with hard coded arguments to test functionality of TicketmasterService.

--- app/src/main/java/com/amerrell/eventsquared/Constants.java
+++ app/src/main/java/com/amerrell/eventsquared/Constants.java
@@ -6,6 +6,7 @@ package com.amerrell.eventsquared;
 public class Constants {
     public static final String TM_API_KEY = BuildConfig.TM_API_KEY;
     public static final String TM_LOCATION_BASE_URL = "https://app.ticketmaster.com/discovery/v2/events.json";
+    public static final String TM_KEY_PARAMETER = "apikey";
     public static final String TM_CITY_PARAMETER = "city";
     public static final String TM_STATE_PARAMETER = "stateCode";
 }
--- app/src/main/java/com/amerrell/eventsquared/ui/MainActivity.java
+++ app/src/main/java/com/amerrell/eventsquared/ui/MainActivity.java
@@ -2,8 +2,16 @@ package com.amerrell.eventsquared.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.amerrell.eventsquared.R;
+import com.amerrell.eventsquared.services.TicketmasterService;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +19,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String city = "portland";
+        String state = "OR";
+
+        getTMEvents(city, state);
+    }
+
+    private void getTMEvents(String city, String state) {
+
+        final TicketmasterService ticketmasterService = new TicketmasterService();
+        ticketmasterService.findTMEvents(city, state, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String jsonData = response.body().string();
+                Log.v("RESPONSE: ", jsonData);
+            }
+        });
     }
 }
