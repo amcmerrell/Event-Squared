a36edbe48258d285d998a1733519ccc6b68b33ce
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-19 11:09:10 -0800

    add TicketmasterService to handle request to Ticketmaster API.

---
+++ app/src/main/java/com/amerrell/eventsquared/services/TicketmasterService.java
@@ -0,0 +1,32 @@
+package com.amerrell.eventsquared.services;
+
+import android.util.Log;
+
+import com.amerrell.eventsquared.Constants;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by Guest on 12/19/16.
+ */
+public class TicketmasterService {
+    public static void findTMEvents(String city, String state, Callback callback) {
+        OkHttpClient client = new OkHttpClient.Builder().build();
+
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Constants.TM_LOCATION_BASE_URL).newBuilder();
+        urlBuilder.addQueryParameter(Constants.TM_KEY_PARAMETER, Constants.TM_API_KEY)
+            .addQueryParameter(Constants.TM_CITY_PARAMETER, city)
+            .addQueryParameter(Constants.TM_STATE_PARAMETER, state);
+        String url = urlBuilder.build().toString();
+        Log.v("URL", url);
+
+        Request request = new Request.Builder().url(url).build();
+
+        Call call = client.newCall(request);
+        call.enqueue(callback);
+    }
+}
