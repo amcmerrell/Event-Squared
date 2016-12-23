4f95824d0b26e9fee81fe6ba20ebd18810c6e878
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-12-22 16:33:11 -0800

    final layout cleanup.

--- app/src/main/java/com/amerrell/eventsquared/ui/MainActivity.java
+++ app/src/main/java/com/amerrell/eventsquared/ui/MainActivity.java
@@ -5,9 +5,11 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.amerrell.eventsquared.Constants;
 import com.amerrell.eventsquared.R;
@@ -44,13 +46,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (view == mMainSearchButton) {
             String searchCity = mCityEditText.getText().toString();
             String searchState = mStateEditText.getText().toString();
-            addToSharedPreferences(searchCity, searchState);
+            if (!(searchCity.length() > 0) || !(searchState.length() > 0)) {
+                Toast.makeText(MainActivity.this, "Please fill out all fields before submitting.", Toast.LENGTH_SHORT).show();
+            } else {
+                addToSharedPreferences(searchCity, searchState);
 
-            Intent intent = new Intent(MainActivity.this, EventListActivity.class);
+                Intent intent = new Intent(MainActivity.this, EventListActivity.class);
 
-            mCityEditText.setText("");
-            mStateEditText.setText("");
-            startActivity(intent);
+                mCityEditText.setText("");
+                mStateEditText.setText("");
+                startActivity(intent);
+            }
         }
     }
 }
